319064640eef7054dafe96eb575e6b82660a5f37
JimyRaul <jcastrejonm@gmail.com>
2021-02-02 19:12:08 -0500

    cambios

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -2,19 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 
-<<<<<<< HEAD
-=======
-
->>>>>>> e34e019fbf052d170783ca3c8d8a2878a5024539
 const routes: Routes = [
   {
     path: 'login',
     component: LoginPageComponent
-<<<<<<< HEAD
   },
-=======
-  }
->>>>>>> e34e019fbf052d170783ca3c8d8a2878a5024539
 ];
 
 @NgModule({
--- src/app/modules/books/books-routing.module.ts
+++ src/app/modules/books/books-routing.module.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-<<<<<<< HEAD
-import { BookListPageComponent } from './pages/book-list-page/book-list-page.component'
-import { BookDetailPageComponent } from './pages/book-detail-page/book-detail-page.component';
-=======
 import { BookListPageComponent } from './pages/book-list-page/book-list-page.component';
 import { BookDetailPageComponent } from './pages/book-detail-page/book-detail-page.component';
-
->>>>>>> e34e019fbf052d170783ca3c8d8a2878a5024539
 const routes: Routes = [
   {
     path: '',
