90c9e98cb990e3f87d997edc0198c184a267f876
JimyRaul <jcastrejonm@gmail.com>
2021-02-04 21:32:13 -0500

    custom menu bar

---
+++ src/app/modules/layout/components/header-admin/header-admin.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import {MenuItem} from 'primeng/api';
+@Component({
+  selector: 'app-header-admin',
+  templateUrl: './header-admin.component.html',
+  styleUrls: ['./header-admin.component.scss']
+})
+export class HeaderAdminComponent implements OnInit {
+
+  items: MenuItem[];
+  constructor() { }
+
+  ngOnInit(): void {
+    this.items = [
+      {
+        label: 'DashBoard',
+        icon: 'pi pi-chart-bar',
+      },
+      {
+        label: 'Libros',
+        icon: 'pi pi-file-o',
+        items: [
+          {
+            label: 'Categorias',
+            icon: 'pi pi-copy'
+          },
+          {
+            label: 'Autores',
+            icon: 'pi pi-user'
+          }
+        ]
+      }
+    ];
+  }
+
+}
